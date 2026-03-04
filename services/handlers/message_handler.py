d403217805843a85520601c08ea715b0f3929129
GERKULE5 <konstantinkochengin44@gmail.com>
2026-03-04 14:42:54 +0700

    feat(publisher): add event types to message_handler

--- services/handlers/message_handler.py
+++ services/handlers/message_handler.py
@@ -12,12 +12,30 @@ class MessageHandler:
         event_type = message.get("event_type")
         title = message.get("title")
         content = message.get("content")
+        id = message.get('id')
+        post_id = message.get('vk_post_id')
         vk_group_ids = message.get("vk_group_ids")
+        isPinned = message.get("isPinned")
         message = f"{title}\n{content}"
 
         if platform_type and event_type and vk_group_ids:
             if 'vk' in platform_type:
-                if event_type == 'upload':
+                if event_type == 'UPLOAD':
                     for group_id in vk_group_ids: 
-                        await self.publisher.uploadPost(group_id, message)
+                        post = await self.publisher.uploadPost(id, group_id, message)
+                        if isPinned is True:
+                            await self.publisher.pinPost(group_id,post.post_id)
+                
+                if event_type == 'EDIT':
+                    await self.publisher.editPost(group_id, post_id, message)
+                
+                if event_type == 'DELETE':
+                    await self.publisher.deletePost(group_id, post_id)
+
+                if event_type == 'RESTORE': 
+                    await self.publisher.restorePost(group_id, post_id)
+                
+                if event_type == 'PIN':
+                    await self.publisher.pinPost(group_id, post_id)
+                        
         
--- services/vk/publish.py
+++ services/vk/publish.py
@@ -27,35 +27,35 @@ class Publisher:
     def get_owner_id(self, group_id: int) -> int:
         return -int(group_id)
 
-    async def uploadPost(self, group_id: int, message: str):
+    async def uploadPost(self, id: int, group_id: int, message: str):
         print('try to upload post')
         try:
             post = self.vk.wall.post(owner_id=self.get_owner_id(group_id), message=message, from_group=1)
             post_id = post['post_id']
-            await self.producer.send('posts', {'status': 'uploaded', 'post_id': post_id})
+            await self.producer.send('posts', {'id': id, 'status': 'uploaded', 'post_id': post_id})
             return post
             
         except Exception as e:
             print(e)
             return None, e
 
-    async def deletePost(self, group_id: int, post_id: int):
+    async def deletePost(self, id: int, group_id: int, post_id: int):
         print('try to delete post')
         try:
             post = self.vk.wall.delete(owner_id=self.get_owner_id(group_id) , post_id=post_id)
-            await self.producer.send('posts', {'status': 'deleted','post_id': post_id, 'code': post})
+            await self.producer.send('posts', {'id': id, 'status': 'deleted','post_id': post_id, 'code': post})
             return post
         
         except Exception as e:
             print(e)
             return None, e        
 
-    async def editPost(self, group_id: int, post_id: int, message: str):
+    async def editPost(self, id: int, group_id: int, post_id: int, message: str):
         print('try to edit post')
         try: 
             post = self.vk.wall.edit(owner_id=self.get_owner_id(group_id), post_id=post_id, message=message)
             post_id = post['post_id']
-            await self.producer.send('posts', {'status': 'edited','post_id': post_id})
+            await self.producer.send('posts', {'id': id, 'status': 'edited','post_id': post_id})
             return post
         
         except Exception as e:
